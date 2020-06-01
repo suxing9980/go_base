a76ae804e5afa6f962d39e02df66343c8884e565
suxing9980 <suxing9980@163.com>
2020-06-01 10:05:49 +0800

    1、字符串去重2

--- 1_slice/1_切片截取数组.go
+++ 1_slice/1_切片截取数组.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	// 总结：
 	// 1、 slice[low:high:max]中，len = high - low，cap = max - low
-	// 2、 切片后的容量不可以超过切片前容量，如果从0位置开始，那么最多等同前面cap
+	// 2、 切片后的容量不可以超过基准数组或者切片容量，如果从0位置开始，那么最多等同前面cap
 	// 3、 基于切片的切片，开始位置是基准切片开始往后读取，不可以超过最初的数组界限
 	var arr [10]int = [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
@@ -19,7 +19,7 @@ func main() {
 	fmt.Println("***************************************")
 
 	// 容量不能超过基准的总量减去low
-	s1 := s[1:7:7]
+	s1 := s[5:7:7]
 	fmt.Println("s1:", s1)
 	fmt.Println("len(s1):", len(s1))
 	fmt.Println("cap(s1):", cap(s1))
