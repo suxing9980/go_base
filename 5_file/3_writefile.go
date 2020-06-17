30ff6899dd1690eae14f75a23673bb87b0ff7e26
suxing9980@163.com <suxing92669288>
2020-06-17 20:25:11 +0800

    write file

---
+++ 5_file/3_writefile.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile("E:/GoWorkSpace/exercise/5_file/write.txt", os.O_CREATE|os.O_CREATE, 0666)
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println("打开文件异常", err.Error())
+	} else {
+		n, err := file.Write([]byte("adawdtrhthytheeferwcrswfg"))
+		if err != nil {
+			fmt.Println("1写入文件异常", err.Error())
+		} else {
+			fmt.Println("1写入ok！", n)
+		}
+		n, err = file.WriteString("中国字")
+		if err != nil {
+			fmt.Println("2写入文件异常", err.Error())
+		} else {
+			fmt.Println("2写入ok！", n)
+		}
+	}
+}
